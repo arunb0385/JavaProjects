7a20dd6da92a91004a336c8630d3dad4d3d9cefc
arunb0385 <159451780+arunb0385@users.noreply.github.com>
2024-04-04 17:22:55 +0530

    Add files via upload
    Atm Machine mini Project by using pojo class and collection framework.

---
+++ MiniProjects/src/AtmMachine/AtmOpInterface.java
@@ -0,0 +1,11 @@
+package AtmMachine;
+
+public interface AtmOpInterface {
+	
+	    public void viewBalance();
+	    public void withdrawAmount(double withdrawAmount);
+	    public void depositAmount(double depositAmount );
+	    public  void viewMiniStatement();
+	
+
+}
